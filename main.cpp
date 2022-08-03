27e8902708f9fc9a62385f2ff56adad2898ebf32
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2022-08-03 12:42:13 +0000

    correct imports

--- main.cpp
+++ main.cpp
@@ -9,4 +9,5 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
-  
+    QQmlApplicationEngine engine;
+    qmlRegi
