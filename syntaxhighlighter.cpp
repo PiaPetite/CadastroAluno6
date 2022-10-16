9559a9f8b07369e084c1ec8a5904347c7277decb
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2022-10-16 12:18:11 +0000

    2.7

--- syntaxhighlighter.cpp
+++ syntaxhighlighter.cpp
@@ -5,4 +5,5 @@
 
 //Q_INVOKABLE QString SyntaxHighlighter::replace(QString format, QString& source, void (*function)(QString &source, int seed, QRegExp regex))
 //{
-//  
+//    QRegExp reg(format);
+//    reg.setMinim
