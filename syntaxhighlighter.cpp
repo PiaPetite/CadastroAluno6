71b192240d6ab54bb70ab3fe00213a30890c542d
PiaPetite <108560780+PiaPetite@users.noreply.github.com>
2023-02-04 22:12:24 +0000

    Update CREATIVE_BRIEF.md

--- syntaxhighlighter.cpp
+++ syntaxhighlighter.cpp
@@ -32,4 +32,5 @@ Q_INVOKABLE QString SyntaxHighlighter::analyse(QString source)
     REPLACE("\\#( |)([^\n]+)", "<h1>"+reg.cap(2)+"</h1>", ("<h1>"+reg.cap(2)+"</h1>").length());
     REPLACE("([\n]{2,})", "<br>", 4);
     REPLACE("([\n])", " ", 1);
-    source="<style>*{direction:rtl; text-align: right;}</style><body dir='
+    source="<style>*{direction:rtl; text-align: right;}</style><body dir='rtl'>" + source + "</body>";
+    retu
